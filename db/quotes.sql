b09a9b7fcca4cdda1fc79aaad862ff13312ac366
ShinDarth <borzifrancesco@gmail.com>
2014-04-10 15:15:05 +0200

    fix encoding in db

--- db/quotes.sql
+++ db/quotes.sql
@@ -1,46 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.1.7
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 10, 2014 alle 15:02
--- Versione del server: 5.1.71-community-log
--- PHP Version: 5.3.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `my_shinworld`
---
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `quotes`
---
+DROP TABLE `quotes`;
 
 CREATE TABLE IF NOT EXISTS `quotes` (
   `id` int(20) unsigned NOT NULL,
-  `quote` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `author` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `quote` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Dump dei dati per la tabella `quotes`
---
 
 INSERT INTO `quotes` (`id`, `quote`, `author`) VALUES
 (1, 'Parlare è facile. Mostrami il codice.', 'Linus Torvalds'),
 (2, 'Mentre in fisica devi capire come è fatto il mondo, in informatica sei tu a crearlo.', 'Linus Torvalds');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
