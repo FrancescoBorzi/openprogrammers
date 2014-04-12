b9c7e8b35def3e96e6559087884adf0bb5fcf601
ShinDarth <borzifrancesco@gmail.com>
2014-04-12 10:29:29 +0200

    added gnu/linux menu

--- webdesign.php
+++ webdesign.php
@@ -39,17 +39,29 @@
             <li><a href="chisiamo.php">Chi siamo</a></li>
             <li class="active"><a href="webdesign.php">WebDesign</a></li>
             <li><a href="portfolio.php">Portfolio</a></li>
-             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Software<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li class="dropdown-header">Francesco Borzì</li>
-                  <li><a target="_blank" href="https://github.com/ShinDarth"><img class="hidden-xs" src="images/GitHub-Mark-32px.png" width="20px"> ShinDarth</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Stefano Borzì</li>
-                  <li><a target="_blank" href="https://github.com/Helias"><img class="hidden-xs" src="images/GitHub-Mark-32px.png" width="20px"> Helias</a></li>
-                </ul>
-              </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Software<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">Francesco Borzì</li>
+                <li><a target="_blank" href="https://github.com/ShinDarth"><img class="hidden-xs" src="images/GitHub-Mark-32px.png" width="20px"> ShinDarth</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Stefano Borzì</li>
+                <li><a target="_blank" href="https://github.com/Helias"><img class="hidden-xs" src="images/GitHub-Mark-32px.png" width="20px"> Helias</a></li>
+              </ul>
+            </li>
             <li><a target="_blank" href="http://shinworld.altervista.org">Blog</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">GNU Linux<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li class="dropdown-header">Dal nostro blog</li>
+                <li><a target="_blank" href="http://shinworld.altervista.org/wordpress/come-formattare-il-proprio-pc-installando-linux/">Come formattare il proprio PC installando Linux</a></li>
+                <li><a target="_blank" href="http://shinworld.altervista.org/wordpress/usare-linux-e-veramente-piu-difficile-rispetto-a-windows-8-motivi-dimostrano-il-contrario/">Usare Linux è più difficile rispetto a Windows?</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Dal web</li>
+                <li><a target="_blank" href="https://www.gnu.org/">Cosa sono GNU e il Software Libero?</a></li>
+                <li><a target="_blank" href="http://wiki.ubuntu-it.org/Sicurezza/Malware">Sicurezza dei sistemi operativi GNU/Linux</a></li>
+              </ul>
+            </li>
             <li><a href="contatti.php">Contatti</a></li>
           </ul>
         </div><!--/.nav-collapse -->
