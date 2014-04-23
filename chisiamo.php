2d505b4c8379f5c56c17d7bcbdc1737d3bed0f24
ShinDarth <borzifrancesco@gmail.com>
2014-04-23 14:36:18 +0200

    useless comments

--- chisiamo.php
+++ chisiamo.php
@@ -10,10 +10,7 @@
 
     <title>OpenProgrammers.it</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
